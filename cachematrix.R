a90cdd9833da78e62e3c953fb3cb0f8b248994f1
Ericka Itang <erickaitang@gmail.com>
2020-11-21 22:31:05 +0800

    Project Assignment 2 R Programming Course John Hopkins University By: Corazon Ericka Mae Itang

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Date: 21 Nov 2020
+# Programming Assignment 2 (Coursera: JHU R Programming)
+# This project contains two pairs of functions
 
-## Write a short comment describing this function
+# The first function creates an R object (a list) that caches
+# 1 the value of a matrix; and
+# 2 the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+# The second function uses the first function as its argument in order to
+# solve the inverse of the matrix x. This is where the solve() function is executed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
